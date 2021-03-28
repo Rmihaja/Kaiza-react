1ae5bdaf787773881f8c70a3bd74269ad9d11fa1
Rmihaja <50056520+Rmihaja@users.noreply.github.com>
2021-03-28 17:23:59 +0200

    🗃 Add more mock data to preview group chat
    Add project github link to Header
    Add global anchor style

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,6 +6,12 @@ const Header = () => {
             <h1 className="title">Kaiza</h1>
             <div className='row'>
                 <button className="profile-picture">MR</button>
+                <a target="_blank" rel="noreferrer" href="https://github.com/rmihaja/kaiza-react/">
+                    <span className="material-icons">
+                        code
+                    </span>
+                    <p>Github</p>
+                </a>
             </div>
         </header>
      );
